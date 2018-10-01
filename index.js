504ab54c471871992cff74f00afc14d20c87349f
Hemil Patel <hemilpatel41196@gmail.com>
2018-10-01 22:33:09 +0530

    initial server setup

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// create express app
+const app = express();
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json())
+
+// define a simple route
+app.get('/', (req, res) => {
+    res.json({"message": "Welcome to my first Node JS REST API named bookstore-api."});
+});
+
+// listen for requests
+app.listen(3000, () => {
+    console.log("Server is listening on port 3000");
+});
