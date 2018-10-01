c30c13a05d141efb0f14fdcc23e4b6df290eeef8
Hemil Patel <hemilpatel41196@gmail.com>
2018-10-01 23:26:57 +0530

    [feature]: Controller setup

---
+++ app/controllers/book.controller.js
@@ -0,0 +1,26 @@
+const Book = require('../models/book.model.js');
+
+// Create and Save a new Book
+exports.create = (req, res) => {
+
+};
+  
+// Retrieve and return all books from the database.
+exports.findAll = (req, res) => {
+
+};
+
+// Find a single book with a bookId
+exports.findOne = (req, res) => {
+
+};
+
+// Update a book identified by the bookId in the request
+exports.update = (req, res) => {
+
+};
+
+// Delete a book with the specified bookId in the request
+exports.delete = (req, res) => {
+
+};
