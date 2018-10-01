d4b4e2edb7e7e6f3b84c93db199eaef98d42c7c8
Hemil Patel <hemilpatel41196@gmail.com>
2018-10-01 22:42:13 +0530

    database configured

--- index.js
+++ index.js
@@ -10,6 +10,20 @@ app.use(bodyParser.urlencoded({ extended: true }))
 // parse requests of content-type - application/json
 app.use(bodyParser.json())
 
+// Configuring the database
+const dbConfig = require('./config/database.config.js');
+const mongoose = require('mongoose');
+
+// Connecting to the database
+mongoose.connect(dbConfig.url, {
+    useNewUrlParser: true
+}).then(() => {
+    console.log("Successfully connected to the database");    
+}).catch(err => {
+    console.log('Could not connect to the database. Exiting now...', err);
+    process.exit();
+});
+
 // define a simple route
 app.get('/', (req, res) => {
     res.json({"message": "Welcome to my first Node JS REST API named bookstore-api."});
